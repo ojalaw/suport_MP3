bf5e7b5642d79edaec2f339a12959b19a08c1594
ojalaw <124128579+ojalaw@users.noreply.github.com>
2023-10-19 18:35:50 +0100

    Minor update to __init__.py, auth.py and views.py
    Imported login_required from flask_login. Login/logout functions working effectively and logout button only appears if users are authenticated and logged in.

--- suport/auth.py
+++ suport/auth.py
@@ -2,16 +2,34 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('You logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -37,4 +55,4 @@ def sign_up():
             flash('You created an account!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign-up.html")
+    return render_template("sign_up.html", user=current_user)
