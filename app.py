ab268f6134726509ab8892f4e40bd75fed152d78
ojalaw <124128579+ojalaw@users.noreply.github.com>
2023-10-20 12:23:49 +0100

    update to app.py
    indent correction

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from suport import flask_app
 app = flask_app()
 
 if __name__ == '__main__':
-     port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
