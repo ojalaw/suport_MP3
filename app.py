ed454f31db9e7081ddb4ac43ff4cf5587b4f926c
ojalaw <124128579+ojalaw@users.noreply.github.com>
2023-10-20 12:21:13 +0100

    Minor update to app.py
    troubleshooting heroku deployment issues

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from suport import flask_app
 app = flask_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
