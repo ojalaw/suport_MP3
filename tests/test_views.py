83807745f269297fae52184d7faae7d23909513b
ojalaw <124128579+ojalaw@users.noreply.github.com>
2023-11-12 11:20:16 +0000

    Minor update to test_views.py
    Modified previous automated testing to reflect changes to views.py, also added more automated testing to test the post and comment features. Had to set up a test user to get passed authentication.

--- tests/test_views.py
+++ tests/test_views.py
@@ -4,10 +4,29 @@ from app import flask_app
 def add_numbers(a, b):
     return a + b
 
-class TestAddition(unittest.TestCase):
+class SanityTest(unittest.TestCase):
     def test_addition(self):
         result = add_numbers(2, 3)
         self.assertEqual(result, 5)
+        
+class TestPostFeature(unittest.TestCase):
+    def setUp(self):
+        self.app = flask_app().test_client()
+        self.login()
+
+    def login(self):
+        return self.app.post('/login', data=dict(
+            username='testuser',
+            password='Testpassword1'
+        ), follow_redirects=True)
+        
+    def test_create_post(self):
+        response = self.app.post('/football-post', data={'post': 'This is a new football post.'})
+        self.assertEqual(response.status_code, 302)
+        
+    def test_add_comment(self):
+        response = self.app.post('/add-comment/football/1', data={'content': 'This is a comment.'})
+        self.assertEqual(response.status_code, 302)
 
 class TestOverviewRoute(unittest.TestCase):
     def setUp(self):
@@ -23,13 +42,13 @@ class TestSportsRoutes(unittest.TestCase):
         self.app = flask_app().test_client()
 
     def test_football_route(self):
-        self.check_route('/football')
+        self.check_route('/football-post')
 
     def test_formula1_route(self):
-        self.check_route('/formula1')
+        self.check_route('/formula1-post')
 
     def test_rugby_route(self):
-        self.check_route('/rugby')
+        self.check_route('/rugby-post')
 
     def check_route(self, route):
         response = self.app.get(route)
